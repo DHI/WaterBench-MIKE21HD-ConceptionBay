fbbee9765a5695e385abb67bcf088974ea2fae66
FrejaTerpPetersen <frtp@dhigroup.com>
2024-12-17 14:24:09 +0100

    Points.dfs0 with capital P

--- .publish/zip_output.py
+++ .publish/zip_output.py
@@ -7,7 +7,7 @@ zip_fn = ".publish/MIKE21HD-ConceptionBay-output.zip"
 
 def zip_repo():
     with zipfile.ZipFile(zip_fn, "w") as z:
-        path_output = "../output"
+        path_output = "output"
         z.write(path_output + "/Area_2014_2018.dfsu",arcname="Area_2014_2018.dfsu")
         z.write(path_output + "/Area_2019_2022.dfsu",arcname="Area_2019_2022.dfsu")
         z.write(path_output + "/Points.dfs0",arcname="Points.dfs0")
